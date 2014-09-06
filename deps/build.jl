dbb6f5cebef0702646d90870392cc20fe82c8905
Tony Kelman <tony@kelman.net>
2014-09-06 07:51:01 -0700

    fix windows installation
    the winrpm library name has changed on the opensuse build service, see https://build.opensuse.org/package/rdiff/windows:mingw:win64/mingw64-freeglut?linkrev=base&rev=8

--- deps/build.jl
+++ deps/build.jl
@@ -2,11 +2,11 @@ using BinDeps
 
 @BinDeps.setup
 
-libglut = library_dependency("libglut", aliases = ["libglut", "freeglut"])
+libglut = library_dependency("libglut", aliases = ["libglut", "freeglut", "libglut-0"])
 
 @windows_only begin
 	using WinRPM
-	provides(WinRPM.RPM, "freeglut", libglut, os = :Windows)
+	provides(WinRPM.RPM, "libglut0", libglut, os = :Windows)
 end
 
 @linux_only begin
